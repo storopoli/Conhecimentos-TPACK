b9121583ef8aa4d52f40dbd14e71a2b23af6d284
Jose Storopoli <thestoropoli@gmail.com>
2021-10-31 13:31:59 -0300

    Data vis nota, tipo e categ de IES.

--- src/data-vis.jl
+++ src/data-vis.jl
@@ -3,25 +3,22 @@ using Arrow
 using CairoMakie
 using DataFrames
 using Statistics
+using AlgebraOfGraphics: density
 
 df = Arrow.Table(joinpath(pwd(), "data", "data.arrow")) |> DataFrame
 
 set_aog_theme!()
 
-tpack = names(df, Between(:QE_I58, :QE_I65))
+nt = [:NT_GER, :NT_FG, :NT_CE]
+tpack = Symbol.(names(df, Between(:QE_I58, :QE_I65)))
 control = ["NU_IDADE"] ∪ names(df, Between(:TP_SEXO_MASC, :QE_I08_NUM))
-nt = ["NT_GER", "NT_FG", "NT_CE"]
-df_nt_long = stack(select(df, ["CO_CATEGAD_PRIVADA", "CO_ORGACAD_NUM"] ∪ nt), nt)
-df_tpack_long = stack(select(df, ["CO_CATEGAD_PRIVADA", "CO_ORGACAD_NUM"] ∪ tpack), tpack)
-df_control_long = stack(select(df, nt ∪ control), nt)
+df_nt_long = stack(select(df, [:CO_CATEGAD_PRIVADA, :CO_ORGACAD_NUM] ∪ nt), nt)
 
 # Density [NT_GER, NT_FG, NT_CE] long como X e Y
-density_nt = data(df_nt_long) *
-    mapping(
-        :value => "";
-        color=:variable => renamer(["NT_GER" => "GER", "NT_CE" => "CE", "NT_FG" => "FG"]) => "Nota ENADE",
-            ) *
-            AlgebraOfGraphics.density()
+density_nt = data(df) *
+    density() *
+    mapping(nt .=> "Nota ENADE") *
+    mapping(; color=dims(1) => renamer(["GER", "CE", "FG"]) => "Nota ENADE")
 
 fg_density_nt = draw(density_nt;
      legend=(; position=:top, titleposition=:left, framevisible=true, padding=5),
@@ -46,34 +43,45 @@ fg_violin_nt = draw(violin_nt;
 
 save(joinpath(pwd(), "figures", "violin_enade.png"), fg_violin_nt; px_per_unit=3)
 
-# Violin TPACK long como X e Y
-# side e color como CO_ORGACAD_NUM com renamer
-violing_tpack_categ = data(df_tpack_long) *
+# Barplot Expectation TPACK com Tipo de IES
+df_tpack_tipo = combine(groupby(df, :CO_CATEGAD_PRIVADA), tpack .=> mean ∘ skipmissing .=> tpack)
+transform!(df_tpack_tipo, :CO_CATEGAD_PRIVADA => ByRow(x -> ifelse(x == 1, "Privada", "Pública")); renamecols=false)
+df_tpack_tipo = permutedims(df_tpack_tipo, 1, "Perguntas QE")
+df_tpack_tipo = stack(df_tpack_tipo, 2:3)
+expectation_tpack_tipo = data(df_tpack_tipo) *
+    visual(BarPlot) *
     mapping(
-        :variable,
+        "Perguntas QE",
         :value => "";
-        color=:CO_CATEGAD_PRIVADA => renamer([0 => "Pública", 1 => "Privada"]) => "Tipo de IES",
-        side=:CO_CATEGAD_PRIVADA => renamer([0 => "Pública", 1 => "Privada"]) => "Tipo de IES"
-        ) *
-    visual(Violin; show_median=true, width=0.95)
+        color=:variable => "Tipo de IES",
+        dodge=:variable
+    )
 
-fg_violing_tpack_categ = draw(violing_tpack_categ;
-    legend=(; position=:bottom, titleposition=:left, framevisible=true, padding=5))
 
-save(joinpath(pwd(), "figures", "violin_tpack_categ.png"), fg_violing_tpack_categ; px_per_unit=3)
+fg_expectation_tpack_tipo = draw(expectation_tpack_tipo;
+    legend=(; position=:bottom, titleposition=:left, framevisible=true, padding=5),
+    axis=(; xticklabelrotation=π/8))
 
-# Violin TPACK long como X e Y
-# dodge e color como CO_ORGACAD_NUM com renamer
-violin_tpack_tipo = data(df_tpack_long) *
+save(joinpath(pwd(), "figures", "barplot_tpack_tipo.png"), fg_expectation_tpack_tipo; px_per_unit=3)
+
+# Barplot Expectation TPACK com Categoria de IES
+df_tpack_categ = combine(groupby(df, :CO_ORGACAD_NUM), tpack .=> mean ∘ skipmissing .=> tpack)
+transform!(df_tpack_categ, :CO_ORGACAD_NUM => ByRow(string); renamecols=false)
+transform!(df_tpack_categ, :CO_ORGACAD_NUM => x -> replace(x, "1" => "Faculdade", "2" => "Centro Universitário", "3" => "Universidade"); renamecols=false)
+df_tpack_categ = permutedims(df_tpack_categ, 1, "Perguntas QE")
+df_tpack_categ = stack(df_tpack_categ, 2:4)
+expectation_tpack_categ = data(df_tpack_categ) *
+    visual(BarPlot) *
     mapping(
-        :variable,
+        "Perguntas QE",
         :value => "";
-        color=:CO_ORGACAD_NUM => renamer([1 => "Faculdade", 2 => "Centro Universitário", 3 => "Universidade"]) => "Categoria de IES",
-        dodge=:CO_ORGACAD_NUM => renamer([1 => "Faculdade", 2 => "Centro Universitário", 3 => "Universidade"]) => "Categoria de IES"
-        ) *
-    visual(Violin; show_median=true, width=0.95)
+        color=:variable => "Categoria de IES",
+        dodge=:variable
+    )
 
-fg_violin_tpack_tipo = draw(violing_tpack_tipo;
-    legend=(; position=:bottom, titleposition=:left, framevisible=true, padding=5))
 
-save(joinpath(pwd(), "figures", "violin_tpack_tipo.png"), fg_violin_tpack_tipo; px_per_unit=3)
+fg_expectation_tpack_categ = draw(expectation_tpack_categ;
+    legend=(; position=:bottom, titleposition=:left, framevisible=true, padding=5),
+    axis=(; xticklabelrotation=π/8))
+
+save(joinpath(pwd(), "figures", "barplot_tpack_categ.png"), fg_expectation_tpack_categ; px_per_unit=3)
