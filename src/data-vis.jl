e6918605fccdbc9d7a13050ae49a6f9e92eb7d44
Jose Storopoli <thestoropoli@gmail.com>
2021-11-03 03:33:40 -0300

    Data vis nota, tipo e categ de IES rev final

--- src/data-vis.jl
+++ src/data-vis.jl
@@ -172,53 +172,98 @@ axislegend(
 save(joinpath(pwd(), "figures", "violin_enade.png"), f)
 
 # AoG NT_GERAL vs TPACK
-base_layers = data(df) * (linear() + visual(Scatter))
+base_layers = data(df) * (linear())
 base_fig = (; resolution=(1600, 1200))
+base_legend = (; position=:bottom, titleposition=:left, framevisible=true, padding=5)
 
 # TPACK TPK
-plt_tpk = base_layers * mapping(:QE_I58, :NT_GER)
+plt_tpk =
+    base_layers * mapping(
+        :QE_I58,
+        :NT_GER;
+        color=:CO_CATEGAD_PRIVADA =>
+            renamer([0 => "Pública", 1 => "Privada"]) => "Tipo de IES",
+        row=:CO_ORGACAD_NUM =>
+            renamer([1 => "Faculdade", 2 => "Centro Universitário", 3 => "Universidade"]),
+    )
 fg_plt_tpk = draw(
-    plt_tpk;
-    figure=base_fig,
-    axis=(;
-        xticks=1:7,
-        title="Notas do ENADE versus Perguntas QE de TPACK - TPK",
-        ylabel="Nota Geral",
-    ),
+    plt_tpk; figure=base_fig, axis=(; xticks=1:7, ylabel="Nota Geral"), legend=base_legend
+)
+supertitle = Label(
+    fg_plt_tpk.figure[0, :],
+    "Notas do ENADE versus Perguntas QE de TPACK - TPK";
+    textsize=30,
+    tellwidth=false,
 )
 
 save(joinpath(pwd(), "figures", "scatter_tpack_tpk.png"), fg_plt_tpk)
 
 # TPACK PCK
-plt_pck = base_layers * mapping(:QE_I38, :NT_GER)
+plt_pck =
+    base_layers * mapping(
+        :QE_I38,
+        :NT_GER;
+        color=:CO_CATEGAD_PRIVADA =>
+            renamer([0 => "Pública", 1 => "Privada"]) => "Tipo de IES",
+        row=:CO_ORGACAD_NUM =>
+            renamer([1 => "Faculdade", 2 => "Centro Universitário", 3 => "Universidade"]),
+    )
 fg_plt_pck = draw(
-    plt_pck;
-    figure=base_fig,
-    axis=(;
-        xticks=1:7,
-        title="Notas do ENADE versus Perguntas QE de TPACK - PCK",
-        ylabel="Nota Geral",
-    ),
+    plt_pck; figure=base_fig, axis=(; xticks=1:7, ylabel="Nota Geral"), legend=base_legend
+)
+supertitle = Label(
+    fg_plt_pck.figure[0, :],
+    "Notas do ENADE versus Perguntas QE de TPACK - PCK";
+    textsize=30,
+    tellwidth=false,
 )
 
 save(joinpath(pwd(), "figures", "scatter_tpack_pck.png"), fg_plt_pck)
 
 # TPACK PC
 pc_vars = [:QE_I28, :QE_I39, :QE_I49, :QE_I57]
-plt_pc = base_layers * mapping(pc_vars, :NT_GER; col=dims(1))
-fg_plt_pc = draw(plt_pc; figure=base_fig, axis=(; xticks=1:7, ylabel="Nota Geral"))
+plt_pc =
+    base_layers * mapping(
+        pc_vars,
+        :NT_GER;
+        col=dims(1),
+        color=:CO_CATEGAD_PRIVADA =>
+            renamer([0 => "Pública", 1 => "Privada"]) => "Tipo de IES",
+        row=:CO_ORGACAD_NUM =>
+            renamer([1 => "Faculdade", 2 => "Centro Universitário", 3 => "Universidade"]),
+    )
+fg_plt_pc = draw(
+    plt_pc; figure=base_fig, axis=(; xticks=1:7, ylabel="Nota Geral"), legend=base_legend
+)
 supertitle = Label(
-    fg_plt_pc.figure[0, :], "Notas do ENADE versus Perguntas QE de TPACK - PC"; textsize=30
+    fg_plt_pc.figure[0, :],
+    "Notas do ENADE versus Perguntas QE de TPACK - PC";
+    textsize=30,
+    tellwidth=false,
 )
 
 save(joinpath(pwd(), "figures", "scatter_tpack_pc.png"), fg_plt_pc)
 
 # TPACK PK
 pk_vars = [:QE_I40, :QE_I29, :QE_I30, :QE_I32, :QE_I36, :QE_I37, :QE_I56, :QE_I38]
-plt_pk = base_layers * mapping(pk_vars, :NT_GER; col=dims(1))
-fg_plt_pk = draw(plt_pk; figure=base_fig, axis=(; xticks=1:7, ylabel="Nota Geral"))
+plt_pk =
+    base_layers * mapping(
+        pk_vars,
+        :NT_GER;
+        col=dims(1),
+        color=:CO_CATEGAD_PRIVADA =>
+            renamer([0 => "Pública", 1 => "Privada"]) => "Tipo de IES",
+        row=:CO_ORGACAD_NUM =>
+            renamer([1 => "Faculdade", 2 => "Centro Universitário", 3 => "Universidade"]),
+    )
+fg_plt_pk = draw(
+    plt_pk; figure=base_fig, axis=(; xticks=1:7, ylabel="Nota Geral"), legend=base_legend
+)
 supertitle = Label(
-    fg_plt_pk.figure[0, :], "Notas do ENADE versus Perguntas QE de TPACK - PK"; textsize=30
+    fg_plt_pk.figure[0, :],
+    "Notas do ENADE versus Perguntas QE de TPACK - PK";
+    textsize=30,
+    tellwidth=false,
 )
 
 save(joinpath(pwd(), "figures", "scatter_tpack_pk.png"), fg_plt_pk)
