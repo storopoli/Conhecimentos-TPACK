878655c3ac36c92068959607aa852cedfd8c6e49
Jose Storopoli <jstoropoli@protonmail.com>
2022-02-19 11:04:45 -0300

    tabelas summary por curso com alunos

--- src/data-summaries.jl
+++ src/data-summaries.jl
@@ -10,26 +10,12 @@ CSV.write(joinpath(pwd(), "tables", "summary.csv"))(
     describe(df, :mean, :median, :q25, :q75, :std, :min, :max)
 )
 
-function rename_cursos(i::Integer)
-    return if i == 1
-        "Administração"
-    elseif i == 2
-        "Direito"
-    elseif i == 12
-        "Medicina"
-    elseif i == 2001
-        "Pedagogia"
-    elseif i == 4004
-        "Ciências da Computação"
-    end
-end
-
 # Por Curso
 q25(x) = quantile(x, 0.25)
 q75(x) = quantile(x, 0.75)
 CSV.write(joinpath(pwd(), "tables", "summary_curso.csv"))(
     combine(
-        groupby(transform(df, :CO_GRUPO => ByRow(rename_cursos) => :CURSO), :CURSO),
-        Between(:NT_GER, :QE_I65) .=> [mean minimum median maximum q25 q75],
+            groupby(df, [:CO_CATEGAD_PRIVADA, :CO_GRUPO]),
+        :CO_GRUPO => length => :QTD_ALUNOS, Between(:NT_GER, :QE_I65) .=> [mean minimum median std maximum q25 q75],
     ),
 )
