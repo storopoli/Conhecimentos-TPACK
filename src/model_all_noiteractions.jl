a24c5bef386ae284c1ab6e93dc3445bd8137fada
Jose Storopoli <jose@storopoli.io>
2022-09-18 11:44:10 -0300

    turing models fails

--- src/model_all_noiteractions.jl
+++ src/model_all_noiteractions.jl
@@ -41,15 +41,15 @@ function recode_curso(x::Int64)
     end
 end
 
-@transform! df begin
+@rtransform! df begin
     :tech = :QE_I58
     :content = :QE_I57
     :pedag = :QE_I29
-    :tech_content = :QE_I58 .* :QE_I57
-    :tech_pedag = :QE_I58 .* :QE_I29
-    :content_pedag = :QE_I57 .* :QE_I29
-    :tech_content_pedag = :QE_I58 .* :QE_I57 .* :QE_I29
-    :curso = @. recode_curso(:CO_GRUPO)
+    # :tech_content = :QE_I58 * :QE_I57
+    # :tech_pedag = :QE_I58 * :QE_I29
+    # :content_pedag = :QE_I57 * :QE_I29
+    # :tech_content_pedag = :QE_I58 * :QE_I57 * :QE_I29
+    :curso = recode_curso(:CO_GRUPO)
 end
 
 # define data matrix X
@@ -73,7 +73,7 @@ idx_privada = df[:, :CO_CATEGAD_PRIVADA] # index is {0=publica, 1=privada}
     # n_gr2=length(unique(idx2)) - 1, # publica is basal, index 0
     mean_y=mean(y),
     std_y=std(y),
-    βᵢ=7, # max index for random slopes
+    βᵢ=3, # max index for random slopes
 )
     # priors
     α ~ TDist(3) * (2.5 * std_y) + mean_y
@@ -83,8 +83,8 @@ idx_privada = df[:, :CO_CATEGAD_PRIVADA] # index is {0=publica, 1=privada}
     # prior for variance of random slopes
     # usually requires thoughtful specification
     τ_1 ~ filldist(truncated(Cauchy(0, 2); lower=0), n_gr1) # group-level SDs slopes
-    Zⱼ_1 ~ filldist(Normal(0, 1), βᵢ, n_gr1)                 # group-level non-centered slopes
-    βⱼ_1 = Zⱼ_1 * τ_1                                       # group-level slopes
+    Zⱼ_1 ~ filldist(Normal(0, 1), βᵢ, n_gr1)                # group-level non-centered slopes
+    βⱼ_1 = Zⱼ_1 .* τ_1                                       # group-level slopes
     # τ_2 ~ filldist(truncated(Cauchy(0, 2); lower=0), n_gr2) # group-level SDs slopes
     # Zⱼ_2 ~ filldist(Normal(0, 1), predictors, n_gr2)        # group-level non-centered slopes
     # βⱼ_2 = Zⱼ_2 * τ_2                                       # group-level slopes
@@ -121,6 +121,21 @@ model = varying_slope_ncp_regression(
 # run chains
 chn = sample(model, NUTS(1_000, 0.8), MCMCThreads(), 1_000, 4)
 
+# get the βⱼs
+gen = generated_quantities(model, chn)
+gen_βⱼs = [i.βⱼ_1 for i in gen]
+gen_βⱼs = vec(gen_βⱼs)
+df_βⱼs = DataFrame(; βⱼs=gen_βⱼs)
+@transform! df_βⱼs $(:βⱼs => AsTable)
+@select! df_βⱼs $([:x1, :x2, :x3] .=> [Symbol("βⱼ[$(i)]") for i in 1:3])
+chn_βⱼs = Array{Float64}(undef, 1_000, 3, 4)
+chn_βⱼs[:, :, 1] = Matrix(df_βⱼs)[1:1_000, :]
+chn_βⱼs[:, :, 2] = Matrix(df_βⱼs)[1_001:2_000, :]
+chn_βⱼs[:, :, 3] = Matrix(df_βⱼs)[2_001:3_000, :]
+chn_βⱼs[:, :, 4] = Matrix(df_βⱼs)[3_001:4_000, :]
+chn_βⱼs = setrange(Chains(chn_βⱼs, [Symbol("βⱼ[$(i)]") for i in 1:3]), 1_001:1:2_000)
+chn = hcat(chn, chn_βⱼs)
+
 # save summarystats
 CSV.write(
     joinpath(pwd(), "results", "all", "turing_summarystats_ger.csv"), summarystats(chn)
@@ -131,19 +146,15 @@ CSV.write(joinpath(pwd(), "results", "all", "turing_quantile_ger.csv"), quantile
 
 # β:
 # 1. QE_I58                    tech
-# 2. QE_I58 * QE_I29           tech_contet
-# 3. QE_I58 * QE_I57           tech_pedag
-# 4. QE_I58 * QE_I29 * QE_I57  tech_content_pedag
-# 5. QE_I29                    content
-# 6. QE_I57 * QE_I29           content_pedag
-# 7. QE_I57                    pedag
-# 8. NU_IDADE                
-# 9. TP_SEXO_MASC
-# 10. QE_I01_SOLTEIRO
-# 11. QE_I02_BRANCA
-# 12. QE_I05_NUM
-# 13. QE_I17_PRIVADO
-# 14. QE_I08_NUM
+# 2. QE_I29                    content
+# 3. QE_I57                    pedag
+# 4. NU_IDADE                
+# 5. TP_SEXO_MASC
+# 6. QE_I01_SOLTEIRO
+# 7. QE_I02_BRANCA
+# 8. QE_I05_NUM
+# 9. QE_I17_PRIVADO
+# 10. QE_I08_NUM
 
 # βⱼ:
 # βⱼ_1: CURSO => 0=adm (basal), 2=direito, 3=medicina, 4=pedagogia, 5=computacao
