cff6559d3940f2d46ae3893c38ae49e22532f4d3
Jose Storopoli <jose@storopoli.io>
2023-01-05 16:45:49 -0300

    PCA context (#7)
    * added model code for PCA
    
    * model run
    
    Co-authored-by: José Storopoli <jose@pumas.ai>

---
+++ src/model_pca_brms.R
@@ -0,0 +1,106 @@
+library(cmdstanr)
+library(brms)
+library(posterior)
+library(arrow)
+library(dplyr)
+library(magrittr)
+library(jsonlite)
+
+# Data
+df <- read_feather("data/data.arrow")
+
+# Variables 
+df %<>% mutate(
+  tech = QE_I58,
+  content = QE_I57,
+  pedag = QE_I29,
+  co_grupo = case_when(
+    CO_GRUPO == 1 ~ 1, # adm
+    CO_GRUPO == 2 ~ 2, # direito
+    CO_GRUPO == 12 ~ 3, # medicina
+    CO_GRUPO == 2001 ~ 4, # pedagogia
+    CO_GRUPO == 4004 ~ 5, # computacao
+  ),
+  tpack_pca = prcomp(
+    as.matrix(cbind(QE_I58, QE_I57, QE_I29)),
+    center = TRUE,
+    scale. = TRUE,
+    rank. = 1
+  )$x[, 1]
+)
+
+# brms formula
+form <- bf(NT_GER ~ 1
+  # fixed effects
+  + tpack_pca
+  # controles
+  + NU_IDADE + TP_SEXO_MASC + QE_I01_SOLTEIRO + QE_I02_BRANCA + QE_I05_NUM
+  + QE_I17_PRIVADO + QE_I08_NUM + CO_REGIAO_CURSO)
+
+mean_NT_GER <- mean(df$NT_GER) # 45.10
+sd_NT_GER <- sd(df$NT_GER) # 14 * 2.5 = 35
+
+# priors
+custom_priors <- c(
+  set_prior("student_t(3, 0, 2.5)", class = "b"),
+  set_prior("student_t(3, 45.1, 35)", class = "Intercept"),
+  set_prior("exponential(1)", class = "sigma")
+)
+
+fit <- brm(form,
+  data = df,
+  family = gaussian(),
+  prior = custom_priors,
+  backend = "cmdstanr",
+  normalize = FALSE,
+  threads = threading(threads = parallel::detectCores()),
+  output_dir = file.path("chains", "model"),
+  cores = 4,
+  chains = 4,
+  iter = 2000
+)
+
+# save model
+make_stancode(
+  form,
+  data = df,
+  family = gaussian(),
+  prior = custom_priors,
+  normalize = FALSE,
+  threads = threading(threads = parallel::detectCores()),
+  cores = 4,
+  chains = 4,
+  iter = 2000
+) %>% writeLines(file.path("src", "model_pca_brms.stan"))
+
+# save data
+make_standata(
+  form,
+  data = df,
+  family = gaussian(),
+  prior = custom_priors,
+  normalize = FALSE,
+  threads = threading(threads = parallel::detectCores()),
+  cores = 4,
+  chains = 4,
+  iter = 2000
+) %>% saveRDS(file.path("data", "stan", "model_pca_brms.rds"))
+
+make_standata(
+  form,
+  data = df,
+  family = gaussian(),
+  prior = custom_priors,
+  normalize = FALSE,
+  threads = threading(threads = parallel::detectCores()),
+  cores = 4,
+  chains = 4,
+  iter = 2000
+) %>%
+  toJSON(pretty = TRUE, auto_unbox = TRUE) %>%
+  write(file.path("data", "stan", "model_pca_brms.json"))
+
+# save results
+(draws_fit <- as_draws_array(fit))
+fit_summary <- summarize_draws(draws_fit)
+fit_summary %>% write.csv("results/results_pca_brms.csv")
---
+++ src/model_pca_brms.stan
@@ -0,0 +1,66 @@
+// generated with brms 2.18.0
+functions {
+  /* integer sequence of values
+   * Args:
+   *   start: starting integer
+   *   end: ending integer
+   * Returns:
+   *   an integer sequence from start to end
+   */
+  int[] sequence(int start, int end) {
+    int seq[end - start + 1];
+    for (n in 1:num_elements(seq)) {
+      seq[n] = n + start - 1;
+    }
+    return seq;
+  }
+  // compute partial sums of the log-likelihood
+  real partial_log_lik_lpmf(int[] seq, int start, int end, data vector Y, data matrix Xc, vector b, real Intercept, real sigma) {
+    real ptarget = 0;
+    int N = end - start + 1;
+    ptarget += normal_id_glm_lupdf(Y[start:end] | Xc[start:end], Intercept, b, sigma);
+    return ptarget;
+  }
+}
+data {
+  int<lower=1> N;  // total number of observations
+  vector[N] Y;  // response variable
+  int<lower=1> K;  // number of population-level effects
+  matrix[N, K] X;  // population-level design matrix
+  int grainsize;  // grainsize for threading
+  int prior_only;  // should the likelihood be ignored?
+}
+transformed data {
+  int Kc = K - 1;
+  matrix[N, Kc] Xc;  // centered version of X without an intercept
+  vector[Kc] means_X;  // column means of X before centering
+  int seq[N] = sequence(1, N);
+  for (i in 2:K) {
+    means_X[i - 1] = mean(X[, i]);
+    Xc[, i - 1] = X[, i] - means_X[i - 1];
+  }
+}
+parameters {
+  vector[Kc] b;  // population-level effects
+  real Intercept;  // temporary intercept for centered predictors
+  real<lower=0> sigma;  // dispersion parameter
+}
+transformed parameters {
+}
+model {
+  real lprior = 0;  // prior contributions to the log posterior
+  // likelihood not including constants
+  if (!prior_only) {
+    target += reduce_sum(partial_log_lik_lpmf, seq, grainsize, Y, Xc, b, Intercept, sigma);
+  }
+  // priors not including constants
+  lprior += student_t_lupdf(b | 3, 0, 2.5);
+  lprior += student_t_lupdf(Intercept | 3, 45.1, 35);
+  lprior += exponential_lupdf(sigma | 1);
+  target += lprior;
+}
+generated quantities {
+  // actual population-level intercept
+  real b_Intercept = Intercept - dot_product(means_X, b);
+}
+
